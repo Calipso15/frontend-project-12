6688d4fa79b2446cdec2407a6655db0ef31559ce
Kris <kris@MacBook-Air-Kris.local>
2024-02-12 10:00:36 +0100

    testerrorrollbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,34 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import init from './init';
 import './index.css';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+import init from './init';
 import initSocket from './sockets';
 
 const socket = initSocket();
 const vdom = init(socket);
 
+const rollbarConfig = {
+  enabled: process.env.NODE_ENV === 'production',
+  accessToken: process.env.ROLLBAR_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+};
+
+function TestError() {
+  const a = null;
+  return a.hello();
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {vdom}
+    <Provider config={rollbarConfig}>
+      <ErrorBoundary>
+        <TestError />
+        {vdom}
+      </ErrorBoundary>
+    </Provider>
   </React.StrictMode>,
 );
--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,8 +2,6 @@ import React from 'react';
 import i18n from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider as StoreProvider } from 'react-redux';
-import { Provider, ErrorBoundary } from '@rollbar/react';
-
 import ru from './locales/ru';
 import store from './redux/store';
 import App from './App';
@@ -23,21 +21,10 @@ const init = () => {
       },
     });
 
-  const rollbarConfig = {
-    enabled: process.env.NODE_ENV === 'production',
-    accessToken: process.env.ROLLBAR_TOKEN,
-    captureUncaught: true,
-    captureUnhandledRejections: true,
-  };
-
   return (
     <I18nextProvider i18n={i18n}>
       <StoreProvider store={store}>
-        <Provider config={rollbarConfig}>
-          <ErrorBoundary>
-            <App />
-          </ErrorBoundary>
-        </Provider>
+        <App />
       </StoreProvider>
     </I18nextProvider>
   );

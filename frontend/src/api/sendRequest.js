26c08a321f256545e356c9097b02efa12e0f3313
Kris <kris@MacBook-Air-Kris.local>
2024-02-14 20:58:25 +0100

    login token

--- frontend/src/api/sendRequest.js
+++ frontend/src/api/sendRequest.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 const sendRequest = (method, endpoint, data, token) => {
   const BASE_URL = '/api/v1';
-  const headers = token ? { Authorization: `Bearer ${token}` } : {};
-
-  if (!data) {
+  const headers = { Authorization: `Bearer ${token}` };
+  if (token === null) {
+    return axios[method](`${BASE_URL}/${endpoint}`, data);
+  }
+  if (data === null) {
     return axios[method](`${BASE_URL}/${endpoint}`, { headers });
   }
   return axios[method](`${BASE_URL}/${endpoint}`, data, { headers });

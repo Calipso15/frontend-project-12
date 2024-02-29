8b333dd4830a9cd7470680163bb81e77234f91d9
Kris <kris@MacBook-Air-Kris.local>
2024-02-29 09:40:37 +0100

    app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,27 +2,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Provider, ErrorBoundary } from '@rollbar/react';
+import { io } from 'socket.io-client';
 import init from './init';
-import initSocket from './sockets';
 
-const socket = initSocket();
-const vdom = init(socket);
-
-const rollbarConfig = {
-  enabled: process.env.NODE_ENV === 'production',
-  accessToken: process.env.ROLLBAR_TOKEN,
-  captureUncaught: true,
-  captureUnhandledRejections: true,
+const app = async () => {
+  const socket = io();
+  const vdom = await init(socket);
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(<React.StrictMode>{vdom}</React.StrictMode>);
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider config={rollbarConfig}>
-      <ErrorBoundary>
-        {vdom}
-      </ErrorBoundary>
-    </Provider>
-  </React.StrictMode>,
-);
+app();
--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import i18n from 'i18next';
+import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import { Provider as StoreProvider } from 'react-redux';
+import { Provider, ErrorBoundary } from '@rollbar/react';
+import { addMessage } from './redux/reducers/messagesSlice';
+import { addChannel, deleteChannel, renameChannel } from './redux/reducers/channelsSlice';
 import ru from './locales/ru';
 import store from './redux/store';
 import App from './App';
 
-const init = () => {
-  i18n
+const init = async (socket) => {
+  const i18n = i18next.createInstance();
+
+  await i18n
     .use(initReactI18next)
     .init({
       resources: {
@@ -21,13 +26,43 @@ const init = () => {
       },
     });
 
-  return (
-    <I18nextProvider i18n={i18n}>
+  socket.on('newMessage', (message) => {
+    store.dispatch(addMessage(message));
+  });
+
+  socket.on('newChannel', (channel) => {
+    store.dispatch(addChannel(channel));
+  });
+
+  socket.on('removeChannel', (data) => {
+    const { id: channelId } = data;
+    store.dispatch(deleteChannel(channelId));
+  });
+
+  socket.on('renameChannel', (data) => {
+    const { id: channelId, name: cleanName } = data;
+    store.dispatch(renameChannel({ name: cleanName, id: channelId }));
+  });
+
+  const rollbarConfig = {
+    enabled: process.env.NODE_ENV === 'production',
+    accessToken: process.env.ROLLBAR_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  };
+
+  const vdom = (
       <StoreProvider store={store}>
-        <App />
+        <Provider config={rollbarConfig}>
+          <ErrorBoundary>
+            <I18nextProvider i18n={i18n}>
+              <App />
+            </I18nextProvider>
+          </ErrorBoundary>
+        </Provider>
       </StoreProvider>
-    </I18nextProvider>
   );
+  return vdom;
 };
 
 export default init;
